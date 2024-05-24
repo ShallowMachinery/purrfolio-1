9589f5f0da3497c4a1e13bff0199598a1fcdee37
ShallowMachinery <eleazar.galope@gmail.com>
2024-05-24 22:45:01 +0800

    5th commit

--- app/page.tsx
+++ app/page.tsx
@@ -89,7 +89,7 @@ export default function Home() {
 
       {/* Inquiries Section */}
       <section id="inquiries" className="w-full h-screen flex flex-col items-center justify-center bg-gray-900 text-gray-100 relative">
-        <div className='border-white border-2 p-32 rounded-lg px-50 min-px-50'>
+        <div className='border-white border-2 p-20 rounded-lg sm:p-10'>
           <h2 className='text-3xl text-center font-bold m-3'>May problema ka ba?</h2>
           <p className='text-large text-center italic m-3'>ra, chatmate tayo</p>
           <p className='text-large text-center italic m-3'>hehe</p>
